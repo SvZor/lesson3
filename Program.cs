7ca64ccc9ae2b90b946625ee4a9ed50da1c480a9
SvZor <zorinasp@yandex.ru>
2024-03-01 00:29:35 +0900

    теория лекции3-1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 ﻿// цикл while который выполняет 5 итераций и выводит на экран квадраты чисел
 // оит одного до 5. Вывод на консоль в строке.
 
-int n = 5;
+// int n = 5;
 
-int i = 1;
-while (i <= n)
+// int i = 1;
+// while (i <= n)
+// {
+//     Console.Write($"{i * i} ");
+//     i = i + 1;
+// }
+
+// int n = 15;
+
+// int i = 1;
+// while (i <= n)
+// {
+//     Console.Write($"{i * i} ");
+//     i = i + 1;
+// }
+// для запуска одного цикла для разных значений используются функции.
+
+// пишем функция для сокращения кода  
+int Square (int value)
 {
-    Console.Write($"{i * i} ");
-    i = i + 1;
+    int result = value * value;
+    return result;
+}
+
+// если функция ничего не возврщает, то используется тип данных void - пустой
+void ShowSquare (int value)
+{
+    int result = value * value;
+    Console.Write(result);
 }
