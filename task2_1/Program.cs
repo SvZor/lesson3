0c596fa0b9bc0a8b9c0b3d6a6f6c5bd673b9bfbe
SvZor <zorinasp@yandex.ru>
2024-03-01 02:06:45 +0900

    ой6

---
+++ task2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 2. вывод на экран квадратов чисел от 1 до N
+
+// void PrintSquares (int limit)
+// {
+//     int i = 1;
+//     while (i <= limit)
+//     {
+//         Console.Write($"{i*i} ");
+//         i = i + 1;
+//     }
+// }
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
+// файл тупит не по детски, я хз что натворила
