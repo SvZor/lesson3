7449c557a6037a6f577032f79d7bc8a17ba4be06
SvZor <zorinasp@yandex.ru>
2024-03-01 00:47:20 +0900

    add lesson3:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 1. вычислить значение (а*б)/(с+д)
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denaminator = c + d;
+    double result = numinator / denaminator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
+
