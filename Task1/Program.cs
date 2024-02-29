807ccdaeda3723419e5182d6727e9b1d966685bb
SvZor <zorinasp@yandex.ru>
2024-03-01 01:57:49 +0900

    ой4

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// задача 1. вычислить значение (а*б)/(с+д)
 
-double CalculateFormula(int a, int b, int c, int d)
-{
-    double numinator = a * b;
-    int denaminator = c + d;
-    double result = numinator / denaminator;
-    return result;
-}
+// double CalculateFormula(int a, int b, int c, int d)
+// {
+//     double numinator = a * b;
+//     int denaminator = c + d;
+//     double result = numinator / denaminator;
+//     return result;
+// }
 
-double result = CalculateFormula(1, 2, 3, 4);
-Console.WriteLine(result);
+// double result = CalculateFormula(1, 2, 3, 4);
+// Console.WriteLine(result);
 
